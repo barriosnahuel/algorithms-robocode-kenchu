12dacdad7e5e22dc75b0da98305add08ac78c3bc
Nahuel Barrios <barrios.nahuel@gmail.com>
2013-08-07 21:20:59 -0300

    Add behavior for escaping from walls.

--- src/main/java/kenchu/Walle.java
+++ src/main/java/kenchu/Walle.java
@@ -16,50 +16,49 @@ import java.awt.*;
  */
 public class Walle extends Robot {
 
-    /**
-     * run: Walle's default behavior
-     */
+    public static final int ROBOT_SIZE = 36;
+
+    @Override
     public void run() {
-        // Initialization of the robot should be put here
 
-        // After trying out your robot, try uncommenting the import at the top,
-        // and the next line:
+        setColors(Color.black, Color.black, Color.green);
 
-        setColors(Color.black, Color.black, Color.green); // body,gun,radar
-
-        // Robot main loop
         while (true) {
-            ahead(100);
-            turnGunRight(360);
-            back(100);
+            ahead(50);
             turnGunRight(360);
         }
     }
 
-    /**
-     * onScannedRobot: What to do when you see another robot
-     */
     @Override
     public void onScannedRobot(ScannedRobotEvent event) {
-        // Replace the next line with any behavior you would like
         fire(1);
     }
 
-    /**
-     * onHitByBullet: What to do when you're hit by a bullet
-     */
     @Override
     public void onHitByBullet(HitByBulletEvent event) {
-        // Replace the next line with any behavior you would like
         back(10);
     }
 
-    /**
-     * onHitWall: What to do when you hit a wall
-     */
     @Override
     public void onHitWall(HitWallEvent event) {
-        // Replace the next line with any behavior you would like
-        back(20);
+        final double degreesToGoOut = 90;
+
+        double bearing = event.getBearing();
+
+        boolean turnRight = true;
+        if (bearing > 0) {
+            turnRight(bearing + degreesToGoOut);
+        } else if (bearing != -180) {
+            turnLeft(bearing * -1 + degreesToGoOut);
+            turnRight = false;
+        }
+
+        ahead(ROBOT_SIZE);
+
+        if (turnRight) {
+            turnRight(degreesToGoOut);
+        } else {
+            turnLeft(degreesToGoOut);
+        }
     }
 }
