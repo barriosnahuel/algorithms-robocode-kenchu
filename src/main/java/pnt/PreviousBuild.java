3903e51dc2b00d37205d154cdc0799b44d1ed7d8
Nahuel Barrios <barrios.nahuel@gmail.com>
2013-08-15 20:40:49 -0300

    Update PreviousBuild.java to revision: 33f865e.

--- src/main/java/pnt/PreviousBuild.java
+++ src/main/java/pnt/PreviousBuild.java
@@ -62,7 +62,13 @@ public class PreviousBuild extends Robot {
 
     private int notFiredBullets = 0;
 
-    private boolean hasTarget;
+    private double distanceToMoveWhenOneOnOne;
+
+    private AttackModes attackMode = AttackModes.EVERYBODY_AGAINST_EVERYBODY;
+
+    private boolean isAttackModeSet;
+
+    private AttackModes previousAttackMode;
 
     @Override
     public void run() {
@@ -71,99 +77,233 @@ public class PreviousBuild extends Robot {
         minimumEnergyToFireBigBullets = initialEnergy * 0.3;
         minimumEnergyToFireSmallestBullets = initialEnergy * 0.1;
 
-        final int degreesToRotateGun = 20;
-
         battleFieldSizeAverage = getBattleFieldWidth() + getBattleFieldHeight() / 2;
         final double distanceToGoAhead = battleFieldSizeAverage / 5;
         final double distanceToGoBack = distanceToGoAhead / 2;
+        distanceToMoveWhenOneOnOne = battleFieldSizeAverage / 3;
 
         setColors(Color.black, Color.black, Color.green);
         setBulletColor(Color.cyan);
 
         //noinspection InfiniteLoopStatement
         while (true) {
-            handleMovement(Direction.AHEAD, distanceToGoAhead);
-            scanForEnemies(degreesToRotateGun, true);
-            handleMovement(Direction.BACK, distanceToGoBack);
-            scanForEnemies(degreesToRotateGun, true);
+            updateAttackModeWhenCorresponding();
+
+            if (attackMode == AttackModes.ONE_ON_ONE) {
+                battleOneOnOne();
+            } else {
+                battleEverybodyAgainstEverybody(distanceToGoAhead, distanceToGoBack);
+            }
         }
     }
 
     /**
-     * Handle moving ahead and back taking into account timing and rotation degrees.
-     *
-     * @param direction
-     *         The {@link Direction} to move.
-     * @param distanceToMove
-     *         The distance to move.
+     * Runs the main strategy when battling against only one enemy.
      */
-    private void handleMovement(Direction direction, double distanceToMove) {
-        final double partialDistanceToMove = ROBOT_SIZE * 2;
+    private void battleOneOnOne() {
+        turnRadarRight(360);
+        ahead(distanceToMoveWhenOneOnOne);
+        turnRadarRight(360);
+        back(distanceToMoveWhenOneOnOne);
+    }
 
-        for (double still = distanceToMove; still > 0; still -= partialDistanceToMove) {
-            switch (direction) {
-                case AHEAD:
-                    ahead(partialDistanceToMove);
-                    break;
-                case BACK:
-                    back(partialDistanceToMove);
-                    break;
-            }
-
-            turnGunLeft(45);
-            //  TODO : Performance : Check degrees
-        }
+    /**
+     * Runs the main strategy when battling against more than one enemy.
+     *
+     * @param distanceToGoAhead
+     *         Distance to move ahead.
+     * @param distanceToGoBack
+     *         Distance to move back
+     */
+    private void battleEverybodyAgainstEverybody(double distanceToGoAhead, double distanceToGoBack) {
+        handleMovement(Direction.AHEAD, distanceToGoAhead);
+        scanForEnemies(40, true);
+        handleMovement(Direction.BACK, distanceToGoBack);
+        scanForEnemies(40, true);
     }
 
     @Override
     public void onScannedRobot(ScannedRobotEvent event) {
-        handleAttackStrategy(event);
+        System.out.println("Scanned robot: " + event.getName() + " (" + event.getEnergy() + ")");
+
+        //  This is in case there are 2 enemies left and one kills the other. Then I see the 1 left but not from the main loop, so...
+        updateAttackModeWhenCorresponding();
+
+        boolean enemyIsNotMoving = event.getVelocity() < 2;
+
+        if (enemyIsNotMoving) {
+            previousAttackMode = attackMode;
+            attackMode = AttackModes.TARGETING;
+        }
+
+        if (attackMode == AttackModes.ONE_ON_ONE) {
+            if (getGunHeat() > 0) {
+                turnGunLeft(5);
+                turnGunRight(10);
+                turnGunLeft(5);
+            }
+
+            attack(event);
+        } else {
+            if (getGunHeat() == 0 || attackMode == AttackModes.TARGETING) {
+                boolean attack = true;
+
+                if (isEscaping) {
+                    //  TODO : Functionality : If I can kill enemy with just one or two bullets, then kill him.
+                    if ((getEnergy() < initialEnergy * 0.3 || event.getEnergy() > initialEnergy * 0.2) && attackMode != AttackModes.ONE_ON_ONE) {
+                        System.out.println("I'm escaping from an enemy, then I won't stay quite to attack another one.");
+                        attack = false;
+                    }
+                }
+
+                if (attack) {
+                    attack(event);
+                }
+            } else {
+                System.out.println("Gun is " + getGunHeat() + " heat: skipping trying to fire.");
+            }
+        }
     }
 
     /**
-     * Handle the attacking strategy when receiving a {@link ScannedRobotEvent}.
+     * Sets the {@link #attackMode} to ONE_ON_ONE if there is only one enemy left and the {@link #isAttackModeSet} field is not set yet.
+     */
+    private void updateAttackModeWhenCorresponding() {
+        if (!isAttackModeSet && getOthers() == 1) {
+            attackMode = AttackModes.ONE_ON_ONE;
+            isAttackModeSet = true;
+        }
+    }
+
+    /**
+     * Fire. Finally this method will execute the call to {@link #fire(double) for each {@link AttackModes}.
+     *
+     * @param event
+     *         The {@link ScannedRobotEvent}.
+     */
+    private void attack(ScannedRobotEvent event) {
+        //  TODO : Functionality : Improve bullet power calculation when one on one or targeting.
+
+        switch (attackMode) {
+            case TARGETING:
+                handleTargeting(event.getBearing());
+                break;
+            case ONE_ON_ONE:
+                handleOneOnOneAttackStrategy(event);
+                break;
+            default:
+                handleFire(calculateBestPowerForShooting(event));
+        }
+    }
+
+    /**
+     * Reorganize radar to the same angle that the gun is and perform a fire/scan action.
+     * <p/>
+     * This method should be called only after first {@link ScannedRobotEvent} in one on one battles.
      *
      * @param event
      *         The The {@link ScannedRobotEvent}.
      */
-    private void handleAttackStrategy(ScannedRobotEvent event) {
-        System.out.println("Scanned robot: " + event.getName() + " (" + event.getEnergy() + ")");
+    private void handleOneOnOneAttackStrategy(ScannedRobotEvent event) {
+        turnRadarToGun(event.getBearing());
 
-        boolean enemyIsNotMoving = event.getVelocity() < 2;
-        if (enemyIsNotMoving && !hasTarget) {
-            hasTarget = true;
+        handleFire(calculateBestPowerForShooting(event));
+    }
+
+    /**
+     * Turn radar left/right to leave it at the same angle that the gun is.
+     *
+     * @param bearing
+     *         The bearing to the enemy.
+     */
+    private void turnRadarToGun(double bearing) {
+        stop(true);
+
+        double firstHeading = getHeading();
+
+        if (Math.abs(bearing) != 90) {
+            setAdjustGunForRobotTurn(true);
+            rotateToHorizontallyAgainst(bearing);
+            setAdjustGunForRobotTurn(false);
         }
 
-        if (getGunHeat() == 0 || hasTarget) {
-            boolean attack = true;
-
-            if (isEscaping) {
-                //  TODO : Functionality : If I can kill enemy with just one or two bullets, then kill him.
-
-                if (getEnergy() < initialEnergy * 0.3 || event.getEnergy() > initialEnergy * 0.2) {
-                    System.out.println("I'm escaping from an enemy, then I won't stay quite to attack another one.");
-                    attack = false;
-                }
-            }
-
-            if (attack) {
-                if (enemyIsNotMoving) {
-                    hasTarget = true;
-                    attackStaticEnemy();
-                } else {
-                    hasTarget = false;
-                    handleFire(calculateBestPowerForShooting(event));
-                }
-            }
+        setAdjustRadarForGunTurn(true);
+        double gunHeading = getGunHeading();
+        if (getHeading() == gunHeading) {
+            turnGunRight(bearing);
         } else {
-            System.out.println("Gun is " + getGunHeat() + " heat: skipping trying to fire.");
+            if (Math.abs(bearing) != 90) {
+                double absolute = firstHeading + bearing;
+                double angle = absolute - gunHeading;
+
+                if (absolute > 0) {
+                    if (angle > 180) {
+                        turnGunLeft(360 - absolute + gunHeading);
+                    } else if (angle > 0) {
+                        //  y menor a 180, por la condicion anterior
+                        turnGunRight(angle);
+                    } else if (angle < 0 && angle > -180) {
+                        turnGunRight(angle);//  da negativo y va para la izquierda
+                    } else if (angle < -180) {
+                        turnGunLeft(360 - gunHeading + absolute);
+                    }
+                } else {
+                    turnGunRight(absolute);
+                }
+            }
         }
+        setAdjustRadarForGunTurn(false);
+
+        if (bearing > 0) {
+            turnRadarLeft(getRadarHeading() - getGunHeading());
+        } else {
+            turnRadarRight(getGunHeading() - getRadarHeading());
+        }
+    }
+
+    /**
+     * Rotates the body of the robot to stay horizontally to the specified robot/wall/bullet origin.
+     *
+     * @param bearing
+     *         The bearing to the robot/wall/bullet origin, in degrees.
+     */
+    private void rotateToHorizontallyAgainst(double bearing) {
+        if (bearing > 0) {
+            turnLeft(90 - bearing);
+        } else {
+            turnRight(90 - Math.abs(bearing));
+        }
+    }
+
+    /**
+     * TODO : Javadoc for changeDirection
+     *
+     * @param bearing
+     * @param degrees
+     *
+     * @return
+     */
+    private boolean changeDirection(double bearing, double degrees) {
+        //  TODO : Performance : Check if this method can be removed and use its code into rotateToHorizontallyAgainst
+        boolean changedRight = true;
+
+        if (bearing > 0) {
+            turnRight(bearing + degrees);
+        } else if (bearing != -180) {
+            turnLeft(bearing * -1 + degrees);
+            changedRight = false;
+        }
+
+        return changedRight;
     }
 
     @Override
     public void onHitByBullet(HitByBulletEvent event) {
         isEscaping = true;
-        hasTarget = false;
+        if (attackMode == AttackModes.TARGETING) {
+            attackMode = previousAttackMode;
+        }
+
         //  TODO : Functionality : If bullet is from the one that is my target, then evaluate continue shooting.
         turnLeft(90 - event.getBearing());
 
@@ -171,9 +311,9 @@ public class PreviousBuild extends Robot {
         double y = getY();
 
         if (isNearWall(x, y) && isHeadingWall(x, y)) {
-            back(DISTANCE_TO_RUN_BACK);
+            back(getDistanceToRun(Direction.BACK));
         } else {
-            ahead(DISTANCE_TO_RUN);
+            ahead(getDistanceToRun(Direction.AHEAD));
         }
 
         isEscaping = false;
@@ -182,19 +322,33 @@ public class PreviousBuild extends Robot {
         turnGunLeft(90 + event.getBearing());
     }
 
+    /**
+     * Calculates the best distance to move the robot for the specified {@code directionToRun} and taking into account the current {@link
+     * #attackMode}.
+     *
+     * @param directionToRun
+     *         Direction indicating where to move.
+     *
+     * @return The distance to move.
+     */
+    private double getDistanceToRun(Direction directionToRun) {
+        //  Default is for run ahead.
+        double distance = DISTANCE_TO_RUN;
+
+        if (attackMode == AttackModes.ONE_ON_ONE) {
+            distance = battleFieldSizeAverage * 0.6;
+        } else if (directionToRun == Direction.BACK) {
+            distance = DISTANCE_TO_RUN_BACK;
+        }
+
+        return distance;
+    }
+
     @Override
     public void onHitWall(HitWallEvent event) {
-        final double degreesToGoOut = 90;
+        rotateToHorizontallyAgainst(event.getBearing());
 
-        boolean turnRight = changeDirection(event.getBearing(), degreesToGoOut);
-
-        ahead(ROBOT_SIZE);
-
-        if (turnRight) {
-            turnRight(degreesToGoOut);
-        } else {
-            turnLeft(degreesToGoOut);
-        }
+        //  TODO : Functionality : Search for enemies after hitting a wall!
     }
 
     @Override
@@ -272,9 +426,16 @@ public class PreviousBuild extends Robot {
      * <p/>
      * <b>Important: </b>If we see a static enemy while we're escaping after receive a bullet from any other enemy, then we fire and continues
      * escaping.
+     *
+     * @param bearing
+     *         The bearing to the enemy.
      */
-    private void attackStaticEnemy() {
+    private void handleTargeting(double bearing) {
         System.out.println("Attacking a static enemy.");
+
+        if (previousAttackMode == AttackModes.ONE_ON_ONE) {
+            turnRadarToGun(bearing);
+        }
 
         handleFire(Rules.MAX_BULLET_POWER);
 
@@ -284,6 +445,7 @@ public class PreviousBuild extends Robot {
             System.out.println("I'm escaping from an enemy, then I won't stay quite to attack another one.");
             resume();
         } else {
+            attackMode = previousAttackMode;
             scan();
         }
     }
@@ -327,44 +489,29 @@ public class PreviousBuild extends Robot {
         double distance = event.getDistance();
         double power = 1;
 
-        if (distance <= battleFieldSizeAverage / 4) {
-            power = Rules.MAX_BULLET_POWER;
-        } else if (getEnergy() < minimumEnergyToFireSmallestBullets) {
-            power = 0.5;
-        } else if (getEnergy() < minimumEnergyToFireBigBullets) {
-            power = 1;
-        } else if (distance <= battleFieldSizeAverage / 3) {
-            power = 2.5;
-        } else if (distance <= battleFieldSizeAverage / 2) {
-            power = 2;
-        }
+        if (attackMode == AttackModes.ONE_ON_ONE) {
+            if (distance <= battleFieldSizeAverage / 3) {
+                power = 3;
+            }
+        } else {
+            if (distance <= battleFieldSizeAverage / 4) {
+                power = Rules.MAX_BULLET_POWER;
+            } else if (getEnergy() < minimumEnergyToFireSmallestBullets) {
+                power = 0.5;
+            } else if (getEnergy() < minimumEnergyToFireBigBullets) {
+                power = 1;
+            } else if (distance <= battleFieldSizeAverage / 3) {
+                power = 2.5;
+            } else if (distance <= battleFieldSizeAverage / 2) {
+                power = 2;
+            }
 
-        if (power == 1 && getOthers() == 1) {
-            power = 2;
+            if (power == 1 && getOthers() == 1) {
+                power = 2;
+            }
         }
 
         return power;
-    }
-
-    /**
-     * TODO : Javadoc for changeDirection
-     *
-     * @param bearing
-     * @param degrees
-     *
-     * @return
-     */
-    private boolean changeDirection(double bearing, double degrees) {
-        boolean changedRight = true;
-
-        if (bearing > 0) {
-            turnRight(bearing + degrees);
-        } else if (bearing != -180) {
-            turnLeft(bearing * -1 + degrees);
-            changedRight = false;
-        }
-
-        return changedRight;
     }
 
     /**
@@ -425,6 +572,32 @@ public class PreviousBuild extends Robot {
         return isHeadingWall;
     }
 
+    /**
+     * Handle moving ahead and back taking into account timing and rotation degrees.
+     *
+     * @param direction
+     *         The {@link Direction} to move.
+     * @param distanceToMove
+     *         The distance to move.
+     */
+    private void handleMovement(Direction direction, double distanceToMove) {
+        final double partialDistanceToMove = ROBOT_SIZE * 2;
+
+        for (double still = distanceToMove; still > 0; still -= partialDistanceToMove) {
+            switch (direction) {
+                case AHEAD:
+                    ahead(partialDistanceToMove);
+                    break;
+                case BACK:
+                    back(partialDistanceToMove);
+                    break;
+            }
+
+            turnGunLeft(45);
+            //  TODO : Performance : Check degrees
+        }
+    }
+
 
     /**
      * Represents the different kind of directions that the robot can take when moving on the battlefild.
@@ -436,5 +609,18 @@ public class PreviousBuild extends Robot {
     public enum Direction {
         AHEAD,
         BACK
+    }
+
+    /**
+     * Represents the different kind of attacks that this robot support.
+     * <p/>
+     * Created on 8/14/13, at 15:36 PM.
+     *
+     * @author Nahuel Barrios <barrios.nahuel@gmail.com>.
+     */
+    public enum AttackModes {
+        TARGETING,
+        ONE_ON_ONE,
+        EVERYBODY_AGAINST_EVERYBODY
     }
 }
